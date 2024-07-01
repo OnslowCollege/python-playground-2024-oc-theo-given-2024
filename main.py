db32fd2269eb85b391fc895428fbe0316747423a
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-01 00:45:49 +0000

    Jul 1, 2024, 12:45 AM

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created by: Theo
 Date: 24 June 2024
 """
 #Setting variables
-totalwinarea = 0.0
+total_win_area = 0.0
 wall1width = -1.0
 wall2width = -1.0
 wallsheight = -1.0
@@ -20,7 +20,7 @@ while wall1width <=0 :
         wall1width = float(input("Enter width of wall 1 (m): "))
     except ValueError:
         print ("Invalid Value")
-#Asking for width of Wall 2
+# Asking for width of Wall 2
 while wall2width <=0 :
     try:
         wall2width = float(input("Enter width of wall 2 (m): "))
@@ -32,7 +32,7 @@ while wallsheight <=0 :
         wallsheight = float(input("Enter height of walls (m): "))
     except ValueError:
         print("Invalid Value")
-#Asking for number of doors and windows
+# Asking for number of doors and windows.
 while doorsnum <=0 and wallsheight >= 2.4:
     try:
         doorsnum = int(input("Enter number of doors: "))
@@ -57,7 +57,7 @@ for i in range(winnum):
         except ValueError:
             print("Invalid Value")
     winarea = winheight * winwidth
-    totalwinarea = totalwinarea + winarea
+    total_win_area = total_win_area + winarea
     winwidth = -1.0
     winheight = -1.0
 #Area Calculations
@@ -67,7 +67,7 @@ wall2area = wall2width * wallsheight
 ceilingarea = wall1width * wall2width
 wallsarea = (wall1area + wall2area) * 2
 #Calculations for paint required and price of whole room
-totalpaint = (wallsarea + ceilingarea - (doorsarea + totalwinarea))/11
+totalpaint = (wallsarea + ceilingarea - (doorsarea + total_win_area)) / 11
 totalprice= totalpaint * 24.68
 #Outputting results
 print ("Total paint required (L): " + str(totalpaint))
