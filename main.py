ede8d2b728c511ff33d6c1c2aa91d2bb27c134e8
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-01 00:48:04 +0000

    Jul 1, 2024, 12:48 AM

--- main.py
+++ main.py
@@ -4,71 +4,71 @@ Main.
 Created by: Theo
 Date: 24 June 2024
 """
-#Setting variables
+# Setting variables.
 total_win_area = 0.0
-wall1width = -1.0
-wall2width = -1.0
-wallsheight = -1.0
-winwidth = -1.0
-winheight = -1.0
-doorsnum = -1
-winnum = -1
-#Asking for width of Wall 1
+wall_1_width = -1.0
+wall_2_width = -1.0
+walls_height = -1.0
+win_width = -1.0
+win_height = -1.0
+doors_num = -1
+win_num = -1
+# Asking for width of Wall 1.
 print("All values must be a number greater than zero.")
-while wall1width <=0 :
+while wall_1_width <=0 :
     try:
-        wall1width = float(input("Enter width of wall 1 (m): "))
+        wall_1_width = float(input("Enter width of wall 1 (m): "))
     except ValueError:
         print ("Invalid Value")
-# Asking for width of Wall 2
-while wall2width <=0 :
+# Asking for width of Wall 2.
+while wall_2_width <=0 :
     try:
-        wall2width = float(input("Enter width of wall 2 (m): "))
+        wall_2_width = float(input("Enter width of wall 2 (m): "))
     except ValueError:
         print("Invalid Value")
-#Asking for height of all Walls
-while wallsheight <=0 :
+# Asking for height of all Walls.
+while walls_height <=0 :
     try:
-        wallsheight = float(input("Enter height of walls (m): "))
+        walls_height = float(input("Enter height of walls (m): "))
     except ValueError:
         print("Invalid Value")
 # Asking for number of doors and windows.
-while doorsnum <=0 and wallsheight >= 2.4:
+while doors_num <=0 and walls_height >= 2.4:
     try:
-        doorsnum = int(input("Enter number of doors: "))
+        doors_num = int(input("Enter number of doors: "))
     except ValueError:
         print("Invalid Value")
-while winnum <=0 :
+while win_num <=0 :
     try:
-        winnum = int(input("Enter number of windows: "))
+        win_num = int(input("Enter number of windows: "))
     except ValueError:
         print("Invalid Value")
-#Windows area and paint calculations
-winheight = wallsheight + 1
-for i in range(winnum):
-    while winheight <= 0 or winheight > wallsheight:
+# Windows area and paint calculations.
+win_height = walls_height + 1
+for i in range(win_num):
+    while win_height <= 0 or win_height > walls_height:
         try:
-            winheight=float(input("Enter window height "+str(i + 1)+": "))
+            win_height=float(input("Enter window height "+str(i + 1)+": "))
         except ValueError:
             print("Invalid Value")
-    while winwidth<=0 or winwidth > (wall1width and wall2width):
+    while win_\width<=0 or win_\width > (wall_1_width and wall_2_width):
         try:
-            winwidth=float(input("Enter window width "+str(i + 1)+": "))
+            win_\width=float(input("Enter window width "+str(i + 1)+": "))
         except ValueError:
             print("Invalid Value")
-    winarea = winheight * winwidth
+    winarea = win_height * win_\width
     total_win_area = total_win_area + winarea
-    winwidth = -1.0
-    winheight = -1.0
-#Area Calculations
-doorsarea = doorsnum * (2.4 * 0.4)
-wall1area = wall1width * wallsheight
-wall2area = wall2width * wallsheight
-ceilingarea = wall1width * wall2width
+    win_\width = -1.0
+    win_height = -1.0
+# Area Calculations.
+doorsarea = doors_num * (2.4 * 0.4)
+wall1area = wall_1_width * walls_height
+wall2area = wall_2_width * walls_height
+ceilingarea = wall_1_width * wall_2_width
 wallsarea = (wall1area + wall2area) * 2
-#Calculations for paint required and price of whole room
+# Calculations for paint required and price of whole room.
 totalpaint = (wallsarea + ceilingarea - (doorsarea + total_win_area)) / 11
 totalprice= totalpaint * 24.68
-#Outputting results
+# Outputting results.
 print ("Total paint required (L): " + str(totalpaint))
 print ("Total price: " + f"${totalprice:.2f}")
