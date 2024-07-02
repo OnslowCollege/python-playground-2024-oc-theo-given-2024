1084059284f48c0142826b861d2a5987ecc7f1c1
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-02 22:14:46 +0000

    Jul 2, 2024, 10:14 PM

---
+++ function_primm_3.py
@@ -0,0 +1,14 @@
+def alternate_case(text: str):
+        build_string: str = ""
+        for i in range(0, len(text)):
+            new_char: str = ""
+            if i % 2 == 0:
+                new_char = text[i].upper()
+            else:
+                new_char += text[i].lower()
+            build_string = build_string + new_char
+        return build_string
+
+
+wibbly_wobbly: str = alternate_case("industrialisation")
+print(wibbly_wobbly)
