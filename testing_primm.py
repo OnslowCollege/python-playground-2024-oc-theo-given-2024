f7ee8b3140abfdb133631964ac82cf1c9778bd72
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-04 21:22:12 +0000

    Jul 4, 2024, 9:22 PM

--- testing_primm.py
+++ testing_primm.py
@@ -1,14 +1,18 @@
-user_input: str = input("What is your age? ")
 
-while:
+user_input: str = ("")
+while user_input == "":
+    user_input = input("What is your age? ")
     try:
         age: int = int(user_input)
         if age <= 0 or age >= 122:
             print("Age incorrect.")
+            user_input = ""
         elif age < 18:
             print("Too young.")
+            user_input = ""
         else:
             print("You may enter")
     except ValueError:
         print("Please enter a number.")
+        user_input = ""
 
