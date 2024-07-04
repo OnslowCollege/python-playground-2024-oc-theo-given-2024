01204fe0cb0e2c86a1c484e57e801aa65202b41c
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-04 21:10:09 +0000

    Jul 4, 2024, 9:10 PM

--- testing_primm.py
+++ testing_primm.py
@@ -4,7 +4,7 @@ try:
     age: int = int(user_input)
     if age <= 0 and age >= 122:
         print("Age incorrect.")
-    elif age <= 18:
+    elif age < 18:
         print("Too young.")
     else:
         print("You may enter")
