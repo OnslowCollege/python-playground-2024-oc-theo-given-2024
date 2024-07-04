e7eddcf94036d2239ad06775222d83abc1e01afa
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-04 21:25:31 +0000

    Jul 4, 2024, 9:25 PM

--- testing_primm.py
+++ testing_primm.py
@@ -1,18 +1,23 @@
-
 user_input: str = ("")
-while user_input == "":
-    user_input = input("What is your age? ")
-    try:
-        age: int = int(user_input)
-        if age <= 0 or age >= 122:
-            print("Age incorrect.")
+age: int
+def ask_for_age():
+    # Setting code to an empty string so the while loop starts
+    while user_input == "":
+        user_input = input("What is your age? ")
+        #Using try in case of errors.
+        try:
+            #Finding the user's age and seeing if it is within the boundaries
+            age = int(user_input)
+            if age <= 0 or age >= 122:
+                print("Age incorrect.")
+                user_input = ""
+            elif age < 18:
+                print("Too young.")
+                user_input = ""
+            else:
+                print("You may enter")
+        except ValueError:
+            #In case of the user not entering a number
+            print("Please enter a number.")
             user_input = ""
-        elif age < 18:
-            print("Too young.")
-            user_input = ""
-        else:
-            print("You may enter")
-    except ValueError:
-        print("Please enter a number.")
-        user_input = ""
 
