b3d00dc1ca3c77c977b585421c8a02d674ef177a
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-22 02:45:17 +0000

    Jul 22, 2024, 2:45 AM

--- lists_primm.py
+++ lists_primm.py
@@ -1,1 +1,12 @@
+# Task 1.
+numbers: list[int] = [1, 2, 3, 4, 5]
+print(numbers[4])
+
+# Task 2.
+fruits: list[str] = ["apple", "banana", "orange"]
+print(len(fruits))
+
+# Task 3.
+colours: list[str] = ["red", "blue", "green"]
+print(f"{colours[-1]} {fruits[0]}")
 
