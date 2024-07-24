33a0148541768a7551915e43a19998b8037649a6
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-24 21:29:05 +0000

    Jul 24, 2024, 9:29 PM

--- dictionaries_primm.py
+++ dictionaries_primm.py
@@ -47,7 +47,7 @@ students_data = {
 }
 
 
-library: dict[] = {
+library: dict[str, dict[str, any]] = {
     "Harry Potter and the Philosopher's Stone": {"author": "J. K. Rowling", "ISBN": 747532745},  # noqa: E501
     "The Lightning Thief": {"author": "Rick Riordan", "ISBN": 9781423121701}
 }
