63e45a27a6eff05fa19708a02cebd0c9bd58781d
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-07-24 21:04:29 +0000

    Jul 24, 2024, 9:04 PM

--- dictionaries_primm.py
+++ dictionaries_primm.py
@@ -51,4 +51,4 @@ library = {
     "Harry Potter and the Philosopher's Stone": {"author": "J. K. Rowling", "ISBN": 747532745}  # noqa: E501
 }
 
-print(library("Harry Potter and the Philosopher's Stone"))
+print(library["Harry Potter and the Philosopher's Stone"])
