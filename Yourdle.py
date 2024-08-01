234820ac492d622421be291e575b4bde37bc8ee6
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-01 20:56:48 +0000

    Aug 1, 2024, 8:56 PM

---
+++ Yourdle.py
@@ -0,0 +1,42 @@
+"""Yourdle is a Wordle clone where you can alter aspects of the game."""
+
+from letter_list_5 import word_list_5
+from letter_list_4 import word_list_4
+
+
+
+#Setting Variables for while loops
+valid_tries = False
+valid_length = False
+
+
+
+#Lists of words of varying lengths
+
+#While loop that forces the user to select a number of guesses within a range.
+while not valid_tries:
+    try:
+        num_tries = int(input("Enter the number of guesses you want (3-10): "))
+        if 3 <= num_tries <= 10:
+            valid_tries = True
+            print("")
+        else:
+            print("")
+            print("Invalid Input.")
+    except ValueError:
+        print("")
+        print("Invalid Input.")
+
+#While loop that forces the user to select a word length within a range.
+while not valid_length:
+    try:
+        word_length = int(input("Enter the length of the word to guess (3-7): "))
+        if 3 <= word_length <= 7:
+            valid_length = True
+            print("")
+        else:
+            print("")
+            print("Invalid Input.")
+    except ValueError:
+        print("")
+        print("Invalid Input.")
---
+++ list_maker.py
@@ -0,0 +1,8 @@
+the_list = input("Enter The list:")
+print("")
+the_better_list = ""
+for i in range(0,len(the_list),5):
+    the_better_list = the_better_list + the_list[i:i+5]
+    the_better_list = the_better_list + " "
+the_best_list = the_better_list.split()
+print(the_best_list)
