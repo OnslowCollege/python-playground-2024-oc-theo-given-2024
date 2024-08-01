3571c9930bb76c726f634639cfe4ace1036fc4e1
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-01 21:22:46 +0000

    Aug 1, 2024, 9:22 PM

--- list_maker.py
+++ list_maker.py
@@ -1,8 +1,8 @@
 the_list = input("Enter The list:")
 print("")
 the_better_list = ""
-for i in range(0,len(the_list),3):
-    the_better_list = the_better_list + the_list[i:i+3]
+for i in range(0,len(the_list),6):
+    the_better_list = the_better_list + the_list[i:i+6]
     the_better_list = the_better_list + " "
 the_best_list = the_better_list.split()
 print(the_best_list)
