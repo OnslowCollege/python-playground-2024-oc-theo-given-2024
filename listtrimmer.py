a65f273647b82008b8a354dc628d6dcfcaef8391
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-06 22:43:27 +0000

    Aug 6, 2024, 10:43 PM

--- listtrimmer.py
+++ listtrimmer.py
@@ -1,9 +1,10 @@
-the_list: list[str] = list(input("Enter the list: "))
-the_better_list = ""
+the_list = input("Enter the list: ")
+the_better_list = the_list.strip()
+the_best_list = ""
 for i in range(len(the_list)):
     print (the_list[i])
     user_input = input("Does this word belong in the list (y/n): ")
     if user_input == "y":
-        the_better_list = the_better_list + the_list[i] + " "
+        the_best_list = the_best_list + the_list[i] + " "
 print("Here is the trimmed list:")
-print(the_better_list.split())
+print(the_best_list.split())
