17cbcb01caccdc10ebe3918b4ce19cbeb8746f58
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-07 21:32:14 +0000

    Aug 7, 2024, 9:32 PM

--- listtrimmer.py
+++ listtrimmer.py
@@ -1,4 +1,6 @@
 the_list = input("Enter the list: ")
+for i in range(2,len(the_list),8):
+    print(the_list[i:i+4])
 the_better_list = the_list.strip()
 the_best_list = ""
 for i in range(len(the_list)):
