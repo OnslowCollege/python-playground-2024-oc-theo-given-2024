02c7748bc7867ed27db677377ac49551c618bfeb
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-12 23:57:34 +0000

    Aug 12, 2024, 11:57 PM

--- Yourdle.py
+++ Yourdle.py
@@ -108,9 +108,9 @@ user_word_list = word_lists[word_length]
 #Randomly selecting a word from the aforementioned list to be the answer.
 word_index = random.randrange(0, len(user_word_list))
 correct_answer = user_word_list[word_index]
-
+correct_answer = correct_answer.upper()
 #Getting the game to tell me the answer (For testing purposes)
-
+print(correct_answer)
 #Puts each character of the correct answer into a dictionary
 correct_chars: dict[str, int] = {}
 for i in range(word_length):
