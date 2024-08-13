04a6e739955ee00bf7aab54d6221c2e8146398a5
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-13 00:10:42 +0000

    Aug 13, 2024, 12:10 AM

--- list_quoter.py
+++ list_quoter.py
@@ -1,5 +1,7 @@
 """Takes a list and puts all entries in double quotes."""
 the_list = input("Enter the list: ")
+the_list = the_list.replace("'", '"')
 print("")
 print("Here is the list:")
-print(the_list.replace("'", '"'))
+for i in range(0, len(the_list), 4095):
+    print(the_list[i:i+4095])
