c865acd4732c48581259e09329df6286f390d23f
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-21 22:38:35 +0000

    Aug 21, 2024, 10:38 PM

--- Yourdle/Yourdle.py
+++ Yourdle/Yourdle.py
@@ -111,7 +111,7 @@ while not valid_length:
 class WordGuess(Static):
     def compose(self) -> ComposeResult:
         for i in range(word_length):
-            yield Static("E", classes = "correct" "letterguess")
+            yield Static("E", classes = "letterguess")
 
 class Yourdle(App):
     CSS_PATH = "Yourdle.tcss"
