c08e5fb589281737475e5212064416c2d4904c4b
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-08-26 23:51:59 +0000

    Aug 26, 2024, 11:51 PM

--- Yourdle/Yourdle.py
+++ Yourdle/Yourdle.py
@@ -122,6 +122,9 @@ class LetterGuess(Static, can_focus = True):
         self.remove_class("correct")
     def on_key(self, event) -> None:
         letter = event.key
+        for i in range(len(letter_list)):
+            if letter == letter_list[i].lower:
+                letter = letter_list[i]
         if letter in letter_list:
             self.update(letter)
             global currentid
