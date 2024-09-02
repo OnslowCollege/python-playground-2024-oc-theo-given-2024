a3aaac778b1c42eb70b5a9da51f515525fd11435
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-09-02 03:03:50 +0000

    Sep 2, 2024, 3:03 AM

--- Yourdle/Yourdle.py
+++ Yourdle/Yourdle.py
@@ -129,6 +129,7 @@ class GuessContainer(Static):
     def compose(self) -> ComposeResult:
         for i in range(num_tries):
             yield WordGuess(id=("c" + str(i)), disabled= True)
+        self.query_one("#c0").disabled = False
 
 class InputContainer(Static):
     def compose(self) -> ComposeResult:
