084f157a067e4684fdfe40adc24cdb273d773b46
oc-theo-given-2024 <theo.given@student.onslow.school.nz>
2024-10-23 19:49:18 +0000

    Oct 23, 2024, 7:49 PM

--- Yourdle/Yourdle.py
+++ Yourdle/Yourdle.py
@@ -233,7 +233,7 @@ class Yourdle(App):
         self.mount(InputContainer())
     def on_win(self, event):
         #Hiding the answer given at the beginning (Will be removed after)
-        self.query_one(CorrectAnswer).add_class("hide")
+        #self.query_one(CorrectAnswer).add_class("hide")
         #Hiding the original shti
         self.query_one(GuessContainer).add_class("hide")
         self.query_one(InputContainer).add_class("hide")
